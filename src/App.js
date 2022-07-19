c3e84d507d5954250f83594e0bcb8e0ae14f74c5
Mallory Daniel <mllr.dnl@gmail.com>
2022-07-19 13:51:30 -0700

    "created checklist page"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import FrontPage from "./pages/FrontPage/FrontPage";
 import NewTrip from "./pages/NewTrip";
+import Checklist from "./pages/Checklist";
 import "./App.css";
 
 document.title = "Pack Way: The smart camping experience";
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<FrontPage />} />
       <Route path="/newtrip" element={<NewTrip />} />
+      <Route path="/checklist" element={<Checklist />} />
     </Routes>
   );
 }
